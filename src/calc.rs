8b3e44109b85f096c4aa62399a922d1b91a7fd4b
Victor Giuliano <143557298+victorgiuliano2162@users.noreply.github.com>
2024-06-20 22:39:54 -0300

    delete enum Op, refactor op_select and implement loop method for operations

--- src/calc.rs
+++ src/calc.rs
@@ -1,23 +1,20 @@
 #[allow(unused)]
 
-
 pub mod calc {
-    use std::{io::{self, Read}, sync::Arc};
-    
-    
-    enum Op {
-        Adic,
-        Sub,
-        Mult,
-        Div,
-        Error
-    }
+    use std::io::{self, Read};
 
     pub fn menu() {
-        println!("Qual a operação desejada?");
-        let mut op = input();
+        println!(
+            "Qual a operação desejada?
+        1 -> adição
+        2 -> subtração
+        3 -> multiplicação
+        4 -> divisão"
+        );
+        let mut inp = input();
 
-        let op_conv = conversor(op);
+        let op_conv = conversor(inp);
+        op_select(op_conv);
     }
 
     fn input() -> String {
@@ -27,47 +24,95 @@ pub mod calc {
     }
 
     fn conversor(mut text: String) -> i32 {
-        io::stdin().read_line(&mut text).expect("converter error");
         let num_format: i32 = text.trim().parse().unwrap();
         num_format
     }
 
-    fn op_selec(op: i32) -> Op{
+    fn op_select(op: i32) {
         match op {
-            1 => Op::Adic,
-            2 => Op::Sub,
-            3 => Op::Mult,
-            4 => Op::Div,
-            _ => Op::Error
+            1 =>  adic(),
+            2 => sub(),
+            3 => mult(),
+            4 => div(),
+            4 => pot(),
+            _ => error(),
         }
     }
 
-    fn oppp(o: Op) {
-        match o {
-            Op::Adic => adic(),
-            Op::Sub => sub(),
-            Op::Mult => mult(),
-            Op::Div => div(),
-            Op::Error => error()
-        }
-    }
+    fn recebe_dois_numeros() -> (i32, i32) {
+        println!("Digite o primeiro número");
+        let num1 = conversor(input());
+        println!("Digite o segundo número");
+        let num2 = conversor(input());
 
-    fn recebe_dois_numeros() {
-
+        (num1, num2)
     }
 
     fn adic() {
+        let (num1, num2) = recebe_dois_numeros();
+        println!(
+            "O resultado da adição entre {} e {} é: {}",
+            num1,
+            num2,
+            num1 + num2
+        )
     }
     fn sub() {
-        
+        let (num1, num2) = recebe_dois_numeros();
+        println!(
+            "O resultado da subtração entre {} e {} é: {}",
+            num1,
+            num2,
+            num1 - num2
+        )
     }
     fn mult() {
-        
+        let (num1, num2) = recebe_dois_numeros();
+        println!(
+            "O resultado da multiplicação entre {} e {} é: {}",
+            num1,
+            num2,
+            num1 * num2
+        )
     }
     fn div() {
-
+        let (num1, num2) = recebe_dois_numeros();
+        println!(
+            "O resultado da divisão entre {} e {} é: {}",
+            num1,
+            num2,
+            num1 / num2
+        )
+    }
+    fn pot() {
+        let (num1, num2) = recebe_dois_numeros();
+        println!(
+            "O resultado da divisão entre {} e {} é: {}",
+            num1,
+            num2,
+            num1 * num2
+        )
+        //TODO: implementar potenciação...
     }
     fn error() {
         println!("Como caralhos rolou um erro???")
+    }
+
+    pub fn nova_op() {
+        loop {
+            println!(
+                "Deseja realizar uma nova operação?
+        1 -> Sim
+        2 -> Não"
+            );
+
+            let inp = conversor(input());
+
+            if inp == 1 {
+                menu()
+            } else {
+                break;
+            }
+        }
     }
 }
